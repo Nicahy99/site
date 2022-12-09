c8242aea7bf6e287049b91a8821179fe4612c306
Nicahy99 <75429559+Nicahy99@users.noreply.github.com>
2022-12-09 01:54:29 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
-var config = {
+/*var config = {
     // Paramètres généraux
     type: Phaser.AUTO, // Type de rendu à utiliser
     width: 1280, // Largeur du jeu en pixels
     height: 720, // Hauteur du jeu en pixels
     parent: 'phaser-example', // Élément HTML parent dans lequel afficher le jeu
-    backgroundColor: '#9adaea', // Couleur d'arrière-plan du jeu
+    backgroundColor: '#000000', // Couleur d'arrière-plan du jeu
     useTicker: true, // Activer le moteur de physique
     fps: 60, // Nombre d'images par seconde souhaité
     pixelArt: true, // Activer le rendu pixelisé
+    scene:{
+        preload: preload,
+        create: create,
+        update: update
+    },
     audio: { // Paramètres audio
         noAudio: false, // Désactiver la prise en charge audio
         disableWebAudio: false, // Désactiver Web Audio API
@@ -22,9 +27,7 @@ var config = {
         delay: 0, // Délai avant la lecture des fichiers audio en millisecondes
         webaudio: { // Paramètres Web Audio API
             context: null, // Contexte audio personnalisé à utiliser
-            ...
         },
-        ...
     },
     physics: { // Paramètres du moteur de physique
         default: 'arcade', // Type de moteur de physique à utiliser
@@ -44,58 +47,52 @@ var config = {
             overlapBias: 4, // Biais de recouvrement pour la détection de collision
         },
     },
-}
+}*/
+
+var config = {
+    // Paramètres généraux
+    type: Phaser.AUTO, // Type de rendu à utiliser
+    width: 1280, // Largeur du jeu en pixels
+    height: 720, // Hauteur du jeu en pixels
+    backgroundColor: '#000000', // Couleur d'arrière-plan du jeu
+    fps: 120, // Nombre d'images par seconde souhaité
+    scene:{
+        preload: preload,
+        create: create,
+        update: update
+    },
+    physics: { // Paramètres du moteur de physique
+        default: 'arcade', // Type de moteur de physique à utiliser
+        arcade: { // Paramètres du moteur de physique arcade
+            gravity: { // Gravité à utiliser
+                x: 0, // Composante x de la gravité en pixels par seconde carrée
+                y: 200 // Composante y de la gravité en pixels par seconde carrée
+            },
+        },
+    },
+};
 
 
-var bullet1;
-var bullet2;
-
-var speed1;
-var speed2;
-
+let perso;
+let cursors;
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('bullet', 'assets/bullet-bill.png');
-    this.load.image('cannon', 'assets/cannon.png');
-    this.load.image('ground', 'assets/ground.png');
     this.load.image('character', 'assets/character.png');
 }
 
 function create ()
-{
-    //   Bullet 1 (600px in 6 seconds)
+{   
+    //      Input
 
-    this.add.image(0, 200, 'ground').setOrigin(0);
+    cursors = this.input.keyboard.createCursorKeys();
 
-    bullet1 = this.add.image(64, 76, 'bullet').setOrigin(0);
-
-    speed1 = Phaser.Math.GetSpeed(600, 6);
-
-    this.add.image(64, 72, 'cannon').setOrigin(0);
-
-    this.add.text(64, 50, '600px / 6 secs', { fill: '#000' });
-
-    //   Bullet 2 (600px in 3 seconds)
-
-    this.add.image(0, 500, 'ground').setOrigin(0);
-
-    bullet2 = this.add.image(64, 376, 'bullet').setOrigin(0);
-
-    speed2 = Phaser.Math.GetSpeed(600, 3);
-
-    this.add.image(64, 500, 'cannon').setOrigin(0, 1);
-
-    this.add.text(64, 350, '600px / 3 secs', { fill: '#000' });
-    
     //      Personnage
     
-    perso = this.physics.add.image(0, 0, 'character').setOrigin(0);
-    
-    perso.setScale(0.5, 0.5);
-    
-    perso.body.enable = true;
+    perso = this.physics.add.image(200, 200, 'character');
+    perso.setScale(0.2, 0.2);
+    perso.body.collideWorldBounds = true;
 }
 
 //  The update function is passed 2 values:
@@ -103,20 +100,23 @@ function create ()
 //  And the delta time, which is derived from the elapsed time since the last frame, with some smoothing and range clamping applied
 
 function update (time, delta)
-{
-    bullet1.x += speed1 * delta;
+{   
+    perso.body.velocity.y += this.physics.world.gravity.y;
+    perso.setVelocityX(0);
 
-    if (bullet1.x > 864)
-    {
-        bullet1.x = 64;
-    }
-
-    bullet2.x += speed2 * delta;
-
-    if (bullet2.x > 864)
-    {
-        bullet2.x = 64;
+    if (cursors.up.isDown){
+        perso.setVelocityY(-500);
     }
     
-    this.perso.body.velocity.y += this.physics.world.gravity.y;
+    if (cursors.down.isDown){
+        perso.setVelocityY(500);
+    }
+    
+    if (cursors.left.isDown){
+        perso.setVelocityX(-500);
+    }
+    
+    if (cursors.right.isDown){
+        perso.setVelocityX(500);
+    }
 }
